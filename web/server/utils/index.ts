a22b0a0127ec0c5a19c39896f654340f404eb587
Taher <76476837+Vallerian@users.noreply.github.com>
2023-10-23 03:32:31 +0330

    Update index.ts

--- web/server/utils/index.ts
+++ web/server/utils/index.ts
@@ -2,7 +2,7 @@ import CryptoJS from 'crypto-js';
 
 export const encrypt = (data: any) => {
     try {
-        const key = process.env.WEBSOCKE_WEB_AUTH_KEY
+        const key = process.env.WEBSOCKET_WEB_AUTH_KEY
         CryptoJS.AES.encrypt(JSON.stringify(data), key).toString()
         return CryptoJS.AES.encrypt(JSON.stringify(data), key).toString()
     } catch (e) {
@@ -12,7 +12,7 @@ export const encrypt = (data: any) => {
 
 export const decrypt = (token: string) => {
     try {
-        const key = process.env.WEBSOCKE_WEB_AUTH_KEY
+        const key = process.env.WEBSOCKET_WEB_AUTH_KEY
         const bytes = CryptoJS.AES.decrypt(token, key)
         return JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
     } catch (ex) {
