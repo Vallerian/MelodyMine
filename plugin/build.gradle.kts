6803f681aae3ab8a66fc9963e41acb7c960f3dfb
Vallerian <tahermoradi8731@gmail.com>
2023-12-19 18:02:40 +0330

    Website update: - Add Turn Server Support

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -33,7 +33,6 @@ repositories {
     maven("https://oss.sonatype.org/content/repositories/snapshots")
     maven("https://oss.sonatype.org/content/repositories/central")
 
-
 //    PlaceholderAPI
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 
@@ -50,10 +49,10 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.10.1")
 
-    implementation("com.github.cryptomorin:XSeries:9.5.0") { isTransitive = false }
+    implementation("com.github.cryptomorin:XSeries:9.7.0") { isTransitive = false }
 
     implementation("net.kyori:adventure-api:4.14.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.3.0")
+    implementation("net.kyori:adventure-platform-bukkit:4.3.1")
     implementation("net.kyori:adventure-text-minimessage:4.14.0")
 
     implementation("cloud.commandframework:cloud-paper:1.9.0-SNAPSHOT")
