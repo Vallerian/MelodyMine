f1d28b5dd13c7abd5b551e6bb22fbeeb95338c52
Vallerian <tahermoradi8731@gmail.com>
2024-01-26 13:34:46 +0330

    fix some issue

--- web/client/src/store/SocketStore.ts
+++ web/client/src/store/SocketStore.ts
@@ -1,20 +1,16 @@
 import {createWithEqualityFn} from "zustand/traditional";
 import {shallow} from "zustand/shallow";
-import {Peer} from "peerjs";
 import {iceServer} from "@/interfaces";
 
 
 interface State {
     socket: any
-    peer: Peer | null
     iceServers: iceServer[]
 }
 
 interface Actions {
     setSocket: (socket: any) => void
-    setPeer: (peer: Peer) => void
     disconnectSocket: () => void
-    disconnectPeer: () => void
     addIceServer: (iceServer: iceServer) => void
 }
 
@@ -28,13 +24,8 @@ export const useSocketStore = createWithEqualityFn<State & Actions>((setState, g
     peer: null,
     addIceServer: (iceServer: iceServer) => setState(() => ({iceServers: [...getState().iceServers, iceServer]})),
     setSocket: socket => setState(() => ({socket})),
-    setPeer: peer => setState(() => ({peer})),
     disconnectSocket: () => {
         getState().socket?.disconnect()
     },
-    disconnectPeer: () => {
-        getState().peer?.destroy()
-        getState().peer?.disconnect()
-    }
 }), shallow)
 
